934cf9993d5562b356e71bf2879022e37ade56fb
MattVogelsang <129625634+MattVogelsang@users.noreply.github.com>
2023-08-13 18:31:36 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const endDate = new Date("September 4, 2023 00:00:00 ").getTime()
+    const endDate = new Date("September 4, 2024 00:00:00 ").getTime()
     const now = new Date().getTime()
 
     const difference = endDate - now
